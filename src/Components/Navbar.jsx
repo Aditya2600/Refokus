a8a1ffbaf4c89cbdfa9e7ee484ba5969d1aa6c7b
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-12-10 21:43:46 +0530

    Work working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,39 +1,30 @@
-import React from "react";
-import Button from "./Button";
+import React from 'react'
+import Button from './Button'
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className="max-w-screen-xl py-6 mx-auto flex items-center justify-between border-b-[1px] border-zinc-700">
-      <div className="nleft flex items-center">
-        <img
-          src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
-          alt=""
-        />
-        <div className="links flex gap-14 ml-20">
-          {["Home", "Work", "Culture", "", "News"].map((elem, index) =>
-            elem.length === 0 ? (
-              <span className="w-[2px] h-7 bg-zinc-700"></span>
-            ) : (
-              <a
-                key={elem}
-                className="text-sm flex items-center gap-1"
-                href="#"
-              >
-                {index === 1 && (
-                  <span
-                    style={{ boxShadow: "0 0 0.55em #00FF19" }}
-                    className="inline-block w-1 h-1 rounded-full bg-green-500"
-                  ></span>
+    <div className='max-w-screen-xl mx-auto py-6 flex items-center justify-between'>
+     <div className='nleft flex items-start'>
+     <img
+            className="mr-12 h-5"
+            src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
+            alt="logo"
+          />
+          <div className='links flex gap-14 ml-20'>
+                {["Home", "Work", "Culture", "", "News"].map((elem,index) => 
+                  elem.length === 0 ? (<span className="w-[2px] h-7 bg-zinc-800 "></span>) : (<a className='text-sm' href='#'>
+                  {index === 1 && <span style={{boxShadow: "0 0 0.45em #00FF19"}} className='inline-block w-1 h-1 text-sm bg-green-500 rounded-full'>
+                    </span>
+                    }
+                  {elem}
+                  </a>
+                  )
                 )}
-                {elem}
-              </a>
-            )
-          )}
-        </div>
-      </div>
-      <Button />
+          </div>
+     </div>
+     <Button />
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
