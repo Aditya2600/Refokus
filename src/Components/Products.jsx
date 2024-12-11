32c981486bab19f972aa2ca458cb484fa822372c
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-12-11 07:56:12 +0530

    Products animation updated

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,40 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./Product";
+import { motion } from "framer-motion";
+import arqitel from "../assets/arqitel.mp4";
+import ttr from "../assets/ttr.mp4";
+import yir from "../assets/yir.mp4";
+import yahoo from "../assets/yahoo.mp4";
+import rainfall from "../assets/rainfall.mp4";
 
 function Products() {
   var products = [
     {
-      title: "arqitel",
+      title: "Arqitel",
       description:
-        "Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores",
+        "With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",
       live: true,
       case: false,
     },
     {
       title: "TTR",
       description:
-        "Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores",
+        "We've created an interactive site using generative AI to allow users to engage with our thinking about Ai, industry trends and design.",
       live: true,
       case: false,
     },
     {
       title: "YIR 2022",
       description:
-        "Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores",
+        "Our second year was filled with great events, exciting projects, awards and amazing people - so we made another showcase to celebrate.",
       live: true,
-      case: true,
+      case: false,
     },
     {
       title: "Yahoo!",
       description:
-        "Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores Arquitectura y diseño de interiores",
+        "We enhanced the New York Fashion Week, by creating a fully digital AR fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models and an integrated web shop.",
       live: true,
-      case: false,
+      case: true,
+    },
+    {
+      title: "Rainfall",
+      description:
+        "We crafted a website for Rainfall Ventures, developing prototypes and custom code that ultimately allows their team to update content regularly and with ease.",
+      live: true,
+      case: true,
     },
   ];
+
+  // Video sources for mapping
+  const videos = [arqitel, ttr, yir, yahoo, rainfall];
+
+  const [pos, setPos] = useState(0);
+  const mover = (val) => {
+    setPos(val * 23);
+  };
+
   return (
-    <div className="mt-32">
-        {products.map((val, index)=> <Product val={val} />)}
+    <div className="mt-32 relative">
+      {products.map((val, index) => (
+        <Product key={index} val={val} mover={mover} count={index} />
+      ))}
+      <div className="absolute top-0 w-full h-full pointer-events-none">
+        <motion.div
+          initial={{ y: pos, x: "-50%" }}
+          animate={{ y: pos + `rem` }}
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.6 }}
+          className="window absolute w-[32rem] h-[23rem] left-[44%] rounded-3xl overflow-hidden"
+        >
+          {videos.map((videoSrc, index) => (
+            <motion.div
+              key={index}
+              animate={{ y: -pos + `rem` }}
+              transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+              className="w-full h-full"
+            >
+              <video
+                className="absolute object-cover rounded-3xl"
+                autoPlay
+                muted
+                loop
+                src={videoSrc}
+              ></video>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
     </div>
   );
 }
